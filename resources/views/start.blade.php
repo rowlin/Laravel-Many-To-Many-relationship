87578d6796b4bf3e9cf3894a5df2bedbbb35b6df
alice <rowlinest90@gmail.com>
2017-08-04 03:24:15 +0300

    comment

---
+++ resources/views/start.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.welcome')
+
+@section('content')
+
+    <div class="row">
+    <h2 style="text-align: center">Laravel Many To Many relationship</h2>
+
+
+        <img  src="{{ url('img/data.png') }}" width="100%">
+
+
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+
+Route::get('discipline', 'AinetController@index')->name('discipline');
+Route::get('new_disc', 'AinetController@new_disc');
+Route::post('create_disc', 'AinetController@create');
+Route::get('edit_disc/{id}', 'AinetController@edit');
+Route::post('update_disc', 'AinetController@update');
+Route::post('delete_disc', 'AinetController@delete');
+Route::post('expulsion', 'AinetController@expulsion');
+
+
+Route::get('users','UserController@index')->name('users');
+Route::get('new_student', 'UserController@new_user');
+Route::post('create_user', 'UserController@create');
+Route::get('edit_user/{id}', 'UserController@edit');
+Route::post('update_user', 'UserController@update');
+Route::post('delete_user', 'UserController@delete');
+
+
+
+
+
+Route::get('/', function () {
+    return view('start');
+});
